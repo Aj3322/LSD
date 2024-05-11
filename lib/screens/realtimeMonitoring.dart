2af1bcf1cfff52e999baf311c4550ba66d17715a
AJAY KUMAR <kumarajay.rs3322@gmail.com>
2024-05-11 11:29:23 +0530

    Updated by Aj

--- lib/screens/realtimeMonitoring.dart
+++ lib/screens/realtimeMonitoring.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 class RealTimeMonitor extends StatefulWidget {
   const RealTimeMonitor({super.key});
 
@@ -11,7 +14,12 @@ class _RealTimeMonitorState extends State<RealTimeMonitor> {
   int counter = 0;
   String emotion = '';
 
-  void imotion() {
+  Future<void> imotion() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _imageFile = image;
+    });
     print(emotion);
     print(counter);
     switch (counter) { // Use integer division to get the quotient
@@ -39,7 +47,11 @@ class _RealTimeMonitorState extends State<RealTimeMonitor> {
     }
 
   }
+   XFile? _imageFile;
 
+  Future<void> _takePicture() async {
+
+  }
 
 
   @override
@@ -50,7 +62,16 @@ class _RealTimeMonitorState extends State<RealTimeMonitor> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text('Emotion  $emotion'),
-            ElevatedButton(onPressed: () => imotion(), child: Text("Monitor"))
+            ElevatedButton(onPressed: () => imotion(), child: Text("Monitor")),
+            Center(
+              child: _imageFile == null
+                  ? Text('No image selected.')
+                  : Image.file(
+                File(_imageFile!.path),
+                height: 300,
+                width: 300,
+              ),
+            ),
           ],
         ),
       ),
